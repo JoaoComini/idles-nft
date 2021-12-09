c1022d29d138091e234fd7b7f64d0c63356e9293
João Comini <joaocomini@gmail.com>
2021-12-08 22:35:24 -0300

    wip

--- dapp/src/components/Game.js
+++ dapp/src/components/Game.js
@@ -1,57 +1,54 @@
 import { useEffect, useState, useCallback } from 'react';
 
-import { Box, Text, Button } from '@chakra-ui/react'
+import {
+    Spacer,
+    Text,
+    Button,
+    Box,
+    Flex,
+    Center,
+    useDisclosure
+} from '@chakra-ui/react'
+
+import Store from './Store';
+
 import { useWeb3React } from '@web3-react/core';
 
 import { idlesToken } from '../contracts/IdlesToken';
-import { itemsCollection } from '../contracts/ItemsCollection';
-import { store } from '../contracts/Store';
 
 function Game() {
-    const { account, library } = useWeb3React()
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { account, library } = useWeb3React();
 
     const [balance, setBalance] = useState(0);
-    const [items, setItems] = useState(0);
 
     const getBalance = useCallback(async () => {
         setBalance(await idlesToken.methods.balanceOf(account).call());
     }, [account]);
 
-    const getItems = useCallback(async () => {
-        console.log(await itemsCollection.methods.balanceOf(account).call());
-        setItems(await itemsCollection.methods.balanceOf(account).call());
-    }, [account]);
-
     useEffect(() => {
         getBalance();
-        getItems();
-    }, [getBalance, getItems]);
+    }, [getBalance]);
 
     const formatedBalance = () => {
         return library.utils.fromWei(balance.toString());
     };
 
-    const handleClick = async () => {
-        const amount = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
-
-        const allowance = await idlesToken.methods.allowance(account, store.options.address).call();
-        
-        if (allowance > 0) {
-            await store.methods.buyChest().send({from: account})
-        } else {
-            await idlesToken.methods.approve(store.options.address, amount).send({from: account})
-        }
-    };
-
     return (
-        <Box>
-            <Text color="white" fontSize="md">
-                {items} Items
-            </Text>
-            <Text color="white" fontSize="md">
-                {formatedBalance()} IDLE
-            </Text>
-            <Button onClick={handleClick}>Buy chest</Button>
+        <Box w='960px' h='540px' bg='gray.300'>
+            <Store isOpen={isOpen} onClose={onClose}/>
+            <Flex h='56px' p={2} bg='gray.600'>
+                <Spacer />
+                <Center mr={4}>
+                    <Text color='white' fontSize='md'>
+                        {formatedBalance()} IDLE
+                    </Text>
+                </Center>
+                <Button onClick={onOpen}>Store</Button>
+            </Flex>
+            <Box w='300px' h='484px' bg='gray.500'>
+
+            </Box>
         </Box>
     );
 }
---
+++ dapp/src/components/Store.js
@@ -0,0 +1,60 @@
+import { useEffect, useState, useCallback } from 'react';
+
+import {
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    Square,
+    VStack,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react';
+
+import { useWeb3React } from '@web3-react/core';
+
+import { idlesToken } from '../contracts/IdlesToken';
+import { store } from '../contracts/Store';
+
+function Store({isOpen, onClose}) {
+    const { account } = useWeb3React();
+
+    const [allowed, setAllowed] = useState(false);
+
+    const getAllowance = useCallback(async () => {
+        const allowance = await idlesToken.methods.allowance(account, store.options.address).call();
+        setAllowed(allowance >= 50 * 1E18);
+    }, [account]);
+
+    useEffect(() => {
+        getAllowance();
+    }, [getAllowance]);
+
+    const handleBuy = async () => {
+        await store.methods.buyChest().send({ from: account });
+    };
+
+    const handleApproval = async () => {
+        const amount = '115792089237316195423570985008687907853269984665640564039457584007913129639935';
+        await idlesToken.methods.approve(store.options.address, amount).send({ from: account });
+    };
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Store</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <VStack>
+                        <Square size='256px' bg='gray.200'></Square>
+                        <Button onClick={handleBuy}>Buy</Button>
+                    </VStack>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
+}
+
+export default Store;
